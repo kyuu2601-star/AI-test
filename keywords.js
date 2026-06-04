c823c9b120d3bda0d33cc796906985778d6c4c1f
kyuu2601-star <kyuu2601@gmail.com>
2026-06-04 11:25:58 +0700

    Create keywords.js

---
+++ keywords.js
@@ -0,0 +1,19 @@
+// keywords.js
+const DALAT_KEYWORDS = {
+    AM_THUC: [
+        "ăn", "uống", "quán", "nhà hàng", "bánh căn", "lẩu", "cà phê", "cf", "cafe", 
+        "ngon", "đói", "thèm", "ăn sáng", "ăn tối", "nhậu", "kem bơ", "gà lá é"
+    ],
+    DI_CHUYEN: [
+        "thuê xe", "xe máy", "ô tô", "taxi", "grab", "xe số", "xe ga", "xăng", 
+        "đổ xăng", "bản đồ", "đường đi", "giao xe", "giá thuê", "bằng lái"
+    ],
+    DEO_DOC: [
+        "đèo", "dốc", "tà nung", "prenn", "mimosa", "ngoạn mục", "bảo lộc", 
+        "nguy hiểm", "sương mù", "tai nạn", "té xe", "trơn", "khuya", "đêm"
+    ],
+    THOI_TIET: [
+        "mưa", "nắng", "lạnh", "thời tiết", "nhiệt độ", "mặc gì", "áo khoác", 
+        "dự báo", "bão", "sương", "ấm", "đồ len", "áo mưa"
+    ]
+};
